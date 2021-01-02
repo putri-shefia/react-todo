d7f9701d1d67b4b11376bd5fa81f85233fdd2a1f
putri-shefia <74754577+putri-shefia@users.noreply.github.com>
2021-01-02 20:12:15 +0700

    Membuat Dummy Object todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const TodoForm = () => {
   return (
-    <section class="add">
-      <form class="add-form">
-        <input class="add-input main-black-color" type="text" />
-        <button class="add-btn main-black-color">Add</button>
+    <section className="add">
+      <form className="add-form">
+        <input className="add-input main-black-color" type="text" />
+        <button className="add-btn main-black-color">Add</button>
       </form>
     </section>
   );
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,9 +2,44 @@ import React from "react";
 import Todo from "./Todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar HTML to React"
+    },
+    {
+      text: "Belajar Props di React"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar State di React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar HTML to React"
+    },
+    {
+      text: "Belajar Props di React"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar State di React"
+    }
+  ];
+
   return (
     <section className="todos">
-      <Todo text="Belajar React" />
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
