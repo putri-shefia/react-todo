b0db837ceeffbdbffaa7a9a97f0de805a7abf9f6
putri-shefia <74754577+putri-shefia@users.noreply.github.com>
2021-01-03 14:03:11 +0700

    addTodo function

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -12,10 +12,18 @@ const TodoList = () => {
     { text: "Learning React Styling" }
   ]);
 
+  const addTodo = (value) => {
+    const addedTodo = [...todos, { text: value }];
+
+    setTodos(addedTodo);
+  };
+
+  console.log("todos", todos);
+
   return (
     <Paper>
       <Header />
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
       <Todos todos={todos} />
     </Paper>
   );
